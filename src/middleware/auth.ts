459cdf14373901dd6d2aa4242a4baa09a35700d9
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-06-18 20:34:10 -0300

    feat: add admin verification

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,27 +2,43 @@ import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
 import { secretKey } from "../../secret";
+import { getUserById } from "../services/userService";
 
-export const authMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  // Check for token in headers
-  const token = req.headers.authorization?.split(" ")[1];
+export const authMiddleware = (requireAdmin = false) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
 
-  if (!token) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Unauthorized" });
-  }
+    if (!authorization) {
+      return res
+        .status(StatusCodes.UNAUTHORIZED)
+        .json({ error: "Authorization header not found" });
+    }
 
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    // Attach the decoded token payload to the request object
-    (req as any).user = decoded;
-    next();
-  } catch (error) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ error: "Invalid token" });
-  }
+    // Check for token in headers
+    const token = authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Unauthorized" });
+    }
+
+    try {
+      const decoded = jwt.verify(token, secretKey) as { id: string; email: string };
+      const user = await getUserById(decoded.id);
+
+      if (!user) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Unauthorized" });
+      }
+
+      // Attach the user to the request object
+      (req as any).user = user;
+
+      if (requireAdmin && user.role !== "ADMIN") {
+        return res.status(StatusCodes.FORBIDDEN).json({ error: "Forbidden: Admins only" });
+      }
+      next();
+    } catch (error) {
+      console.error(error); // Log the error for debugging purposes
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid token" });
+    }
+  };
 };
