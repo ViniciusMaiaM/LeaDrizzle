0f9d792933d51159530be3c16bf1afcda07cc782
Vinicius Maia <viniciusmaiamarinho1@gmail.com>
2024-06-18 20:31:28 -0300

    refactor: remove get all and passing middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -8,29 +8,25 @@ import {
   getUserPosts,
 } from "../controllers/userController";
 import { validateData } from "../middleware/validationMiddleware";
-import {
-  createUserSchema,
-  updateUserSchema,
-} from "../schemas/userSchema";
+import { createUserSchema, updateUserSchema } from "../schemas/userSchema";
 import { authMiddleware } from "../middleware/auth";
 
 const userRoutes = Router();
 
-userRoutes.get("/", authMiddleware, getAllUsers);
-
-userRoutes.get("/:id", authMiddleware, getUserById);
-
-userRoutes.get("/:id/posts", authMiddleware, getUserPosts);
-
 userRoutes.post("/", validateData(createUserSchema), createUser);
+
+userRoutes.use(authMiddleware());
+
+userRoutes.get("/:id", getUserById);
+
+userRoutes.get("/:id/posts", getUserPosts);
 
 userRoutes.put(
   "/:id",
   validateData(updateUserSchema),
-  authMiddleware,
   updateUser
 );
 
-userRoutes.delete("/:id", authMiddleware, deleteUser);
+userRoutes.delete("/:id", deleteUser);
 
 export default userRoutes;
